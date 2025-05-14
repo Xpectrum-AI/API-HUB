7cd9d89e69ebfe3d9751365db18970e7b3cdce79
tirumala-karthikeya <96166812+tirumala-karthikeya@users.noreply.github.com>
2025-05-15 02:15:49 +0530

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import { AuthProvider } from "@propelauth/react"
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { AuthProvider } from "@propelauth/react"
 
-createRoot(document.getElementById("root")!).render(
-  <AuthProvider authUrl={import.meta.env.VITE_AUTH_URL}>
-    <App />
-  </AuthProvider>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AuthProvider authUrl={import.meta.env.VITE_PROPELAUTH_AUTH_URL}>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+)
